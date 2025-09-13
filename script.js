03e59dee8192944f55a0e39f9ac24f98cad0b071
Kaua Gomes <kaua.bezerra7@aluno.ce.gov.br>
2025-09-13 18:58:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -64,70 +64,71 @@ if (pedido == 5) {
 (3) Refrigerante – R$ 5
 (4) Batata Frita – R$ 12
 (5) Sair`))
-    if (pedido3 == 5) {
-        alert("Muito obrigado volte sempre!")
-    } else {
-        switch (pedido3) {
-            case 1:
-                pedido3 = "Hambúguer"
-                preço3 = 20
-                break
-            case 2:
-                pedido3 = "Pizza"
-                preço3 = 35
-                break
-            case 3:
-                pedido3 = "Refrigerante"
-                preço3 = 5
-                break
-            case 4:
-                pedido3 = "Batata Frita"
-                preço3 = 12
-                break
-        }
-        let estudante = prompt("Você é estudante? se sim digite S se não digite N").toLowerCase()
-        let soma = preço1 + preço2 + preço3
-        if (estudante == "s" && soma > 50) {
-            let calculo = soma * 10 / 100
-            let calculo2 = soma * 5 / 100
-            let calculo3 = calculo + calculo2
-            let calculo4 = soma - calculo3
-            alert(`Obrigado ${nome}!
+        if (pedido3 == 5) {
+            alert("Muito obrigado volte sempre!")
+        } else {
+            switch (pedido3) {
+                case 1:
+                    pedido3 = "Hambúguer"
+                    preço3 = 20
+                    break
+                case 2:
+                    pedido3 = "Pizza"
+                    preço3 = 35
+                    break
+                case 3:
+                    pedido3 = "Refrigerante"
+                    preço3 = 5
+                    break
+                case 4:
+                    pedido3 = "Batata Frita"
+                    preço3 = 12
+                    break
+            }
+            let estudante = prompt("Você é estudante? se sim digite S se não digite N").toLowerCase()
+            let soma = preço1 + preço2 + preço3
+            if (estudante == "s" && soma > 50) {
+                let calculo = soma * 10 / 100
+                let calculo2 = soma * 5 / 100
+                let calculo3 = calculo + calculo2
+                let calculo4 = soma - calculo3
+                alert(`Obrigado ${nome}!
             Pratos escolhidos:
             ${pedido} ${preço1} 
             ${pedido2} ${preço2}
             ${pedido3} ${preço3}
             Total sem desconto: ${soma}
             Total com desconto: ${calculo4}`)
-        } else if (soma > 50) {
-            let calculo = soma * 5 / 100
-            let resultado = soma - calculo
-            alert(`Obrigado ${nome}!
+            } else if (soma > 50) {
+                let calculo = soma * 5 / 100
+                let resultado = soma - calculo
+                alert(`Obrigado ${nome}!
             Pratos escolhidos:
             ${pedido} ${preço1} 
             ${pedido2} ${preço2}
             ${pedido3} ${preço3}
             Total sem desconto: ${soma}
             Total com desconto: ${resultado}`)
-        } else if (estudante == "s") {
-            let calculo = soma * 10 / 100
-            let resultado = soma - calculo
-            alert(`Obrigado ${nome}!
+            } else if (estudante == "s") {
+                let calculo = soma * 10 / 100
+                let resultado = soma - calculo
+                alert(`Obrigado ${nome}!
             Pratos escolhidos:
             ${pedido} ${preço1} 
             ${pedido2} ${preço2}
             ${pedido3} ${preço3}
             Total sem desconto: ${soma}
             Total com desconto: ${resultado}`)
-        } else {
-            alert(`Obrigado ${nome}!
+            } else {
+                alert(`Obrigado ${nome}!
             Pratos escolhidos:
             ${pedido} ${preço1} 
             ${pedido2} ${preço2}
             ${pedido3} ${preço3}
             Total sem desconto: ${soma}
             Total com desconto: ${soma}`)
-        }
+            }
 
+        }
     }
-}}
+}
