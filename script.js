91ae3fd18b21c850a4d945bd7eb477e20a41b5bd
Kaua Gomes <kaua.bezerra7@aluno.ce.gov.br>
2025-09-19 14:28:20 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -16,19 +16,19 @@ if (pedido == 5) {
     switch (pedido) {
         case 1:
             pedido = "Hambúrguer"
-            preço1 = 20
+            preço1 = 20.00
             break
         case 2:
             pedido = "Pizza"
-            preço1 = 35
+            preço1 = 35.00
             break
         case 3:
             pedido = "Refrigerante"
-            preço1 = 5
+            preço1 = 5.00
             break
         case 4:
             pedido = "Batata Frita"
-            preço1 = 12
+            preço1 = 12.00
             break
     }
     let pedido2 = parseInt(prompt(`Menu
@@ -43,19 +43,19 @@ if (pedido == 5) {
         switch (pedido2) {
             case 1:
                 pedido2 = "Hambúguer"
-                preço2 = 20
+                preço2 = 20.00
                 break
             case 2:
                 pedido2 = "Pizza"
-                preço2 = 35
+                preço2 = 35.00
                 break
             case 3:
                 pedido2 = "Refrigerante"
-                preço2 = 5
+                preço2 = 5.00
                 break
             case 4:
                 pedido2 = "Batata Frita"
-                preço2 = 12
+                preço2 = 12.00
                 break
         }
         let pedido3 = parseInt(prompt(`Menu
@@ -70,28 +70,28 @@ if (pedido == 5) {
             switch (pedido3) {
                 case 1:
                     pedido3 = "Hambúguer"
-                    preço3 = 20
+                    preço3 = 20.00
                     break
                 case 2:
                     pedido3 = "Pizza"
-                    preço3 = 35
+                    preço3 = 35.00
                     break
                 case 3:
                     pedido3 = "Refrigerante"
-                    preço3 = 5
+                    preço3 = 5.00
                     break
                 case 4:
                     pedido3 = "Batata Frita"
-                    preço3 = 12
+                    preço3 = 12.00
                     break
             }
             let estudante = prompt("Você é estudante? se sim digite S se não digite N").toLowerCase()
-            let soma = preço1 + preço2 + preço3
+            let soma = parseFloat (preço1 + preço2 + preço3)
             if (estudante == "s" && soma > 50) {
-                let calculo = soma * 10 / 100
-                let calculo2 = soma * 5 / 100
-                let calculo3 = calculo + calculo2
-                let calculo4 = soma - calculo3
+                let calculo = parseFloat (soma * 10 / 100)
+                let calculo2 = parseFloat (soma * 5 / 100)
+                let calculo3 = parseFloat (calculo + calculo2)
+                let calculo4 = parseFloat (soma - calculo3)
                 alert(`Obrigado ${nome}!
             Pratos escolhidos:
             ${pedido} ${preço1} 
@@ -99,34 +99,36 @@ if (pedido == 5) {
             ${pedido3} ${preço3}
             Total sem desconto: ${soma}
             Total com desconto: ${calculo4}`)
-            } else if (soma > 50) {
-                let calculo = soma * 5 / 100
-                let resultado = soma - calculo
+            } else if (soma > 50 && estudante == "n") {
+                let calculo = parseFloat (soma * 5 / 100)
+                let resultado = parseFloat( soma - calculo)
                 alert(`Obrigado ${nome}!
             Pratos escolhidos:
-            ${pedido} ${preço1} 
-            ${pedido2} ${preço2}
-            ${pedido3} ${preço3}
-            Total sem desconto: ${soma}
-            Total com desconto: ${resultado}`)
-            } else if (estudante == "s") {
-                let calculo = soma * 10 / 100
-                let resultado = soma - calculo
+            ${pedido} R$${preço1} 
+            ${pedido2} R$${preço2}
+            ${pedido3} R$${preço3}
+            Total sem desconto: R$${soma}
+            Total com desconto: R$${resultado}`)
+            } else if (estudante == "s" && soma <= 50) {
+                let calculo = parseFloat( soma * 10 / 100)
+                let resultado = parseFloat (soma - calculo)
                 alert(`Obrigado ${nome}!
             Pratos escolhidos:
-            ${pedido} ${preço1} 
-            ${pedido2} ${preço2}
-            ${pedido3} ${preço3}
-            Total sem desconto: ${soma}
-            Total com desconto: ${resultado}`)
-            } else {
+            ${pedido} R$${preço1} 
+            ${pedido2} R$${preço2}
+            ${pedido3} R$${preço3}
+            Total sem desconto: R$${soma}
+            Total com desconto: R$${resultado}`)
+            } else if(estudante == "n" && soma <= 50){
                 alert(`Obrigado ${nome}!
             Pratos escolhidos:
-            ${pedido} ${preço1} 
-            ${pedido2} ${preço2}
-            ${pedido3} ${preço3}
-            Total sem desconto: ${soma}
-            Total com desconto: ${soma}`)
+            ${pedido} R$${preço1} 
+            ${pedido2} R$${preço2}
+            ${pedido3} R$${preço3}
+            Total sem desconto: R$${soma}
+            Total com desconto: R$${soma}`)
+            }else{
+                alert("algo deu errado, reinicie a pagina")
             }
 
         }
